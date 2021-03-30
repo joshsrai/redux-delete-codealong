33176653b237e1be3b44b0f91aac9555914ddfef
joshsrai <joshsrai@gmail.com>
2021-03-30 17:34:08 -0400

    commit tree d9b6c2074c70d35b8e5721e5e3115fb294f4427f

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+// const Todo = props => <li>{props.text}</li>
 
-export default Todo;
+const Todo = props => {
+  return (
+    <div>
+      {/* <span>{props.text}</span><button>DELETE</button> */}
+      {/* <span>{props.text}</span><button onClick={() => props.delete(props.text)}>DELETE</button> */}
+      <span>{props.todo.text}</span><button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+    </div>
+  )
+}
+
+export default Todo; 
+ 
